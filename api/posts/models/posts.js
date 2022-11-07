9508a0cf21f0b01278c711a37fd9cb766f1b529e
Ruan Mello <ruan.mello0123@gmail.com>
2022-11-07 12:52:19 -0300

    Adicionando lifecycle hook

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const axios = require('axios');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post('https://api.netlify.com/build_hooks/6367ff048c085d7ea7d87a94');
+  },
+
+  async afterUpdate(result, params, data) {
+    axios.post('https://api.netlify.com/build_hooks/6367ff048c085d7ea7d87a94');
+  }
+};
